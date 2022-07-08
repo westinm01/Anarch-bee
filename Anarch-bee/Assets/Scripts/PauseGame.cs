e5ed990c690d58874bea1a85c2f5a80bbd7e62ca
Wes <wmont012@ucr.edu>
2022-07-08 15:29:03 -0700

    Countdown leads to game over

--- Anarch-bee/Assets/Scripts/PauseGame.cs
+++ Anarch-bee/Assets/Scripts/PauseGame.cs
@@ -6,27 +6,42 @@ public class PauseGame : MonoBehaviour
 {
     public static bool gameIsPaused = false;
     public GameObject pauseMenu;
+    public GameObject levelOver;
+    public GameObject gameOver;
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            Pause();
+            Pause(0);
         }
     }
     
-    public void Pause()
+    public void Pause(int val)
     {
-        gameIsPaused = !gameIsPaused;
-        pauseMenu.SetActive(gameIsPaused);
-        if(gameIsPaused)
-        {
+        switch(val){
+            case 0://pause menu
+            gameIsPaused = !gameIsPaused;
+            pauseMenu.SetActive(gameIsPaused);
+            if(gameIsPaused)
+                {
+                    Time.timeScale = 0f;
+                }
+            else 
+                {
+                    Time.timeScale = 1;
+            }
+            break;
+            case 1://levelOver menu
+            levelOver.SetActive(true);
             Time.timeScale = 0f;
+            break;
+            case 2: //gameOver menu
+            gameOver.SetActive(true);
+            Time.timeScale = 0f;
+            break;
         }
-        else 
-        {
-            Time.timeScale = 1;
-        }
+        
     }
     public void setGameIsPaused(bool val)
     {
