81ea2b5de27ca9640f7295ec52aca374083b7bfd
NikOhNo <80977626+NikOhNo@users.noreply.github.com>
2022-07-08 19:40:44 -0700

    created a mock level

--- Anarch-bee/Assets/Scripts/ScoreKeeper.cs
+++ Anarch-bee/Assets/Scripts/ScoreKeeper.cs
@@ -11,6 +11,7 @@ public class ScoreKeeper : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        FindObjectOfType<TMP_Text>();
         scoreText.text = "Score: " + score.ToString();
     }
 
