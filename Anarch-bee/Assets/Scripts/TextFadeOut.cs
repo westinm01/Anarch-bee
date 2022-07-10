86ccb342f45f35a96e5545b1822f375922a935cb
NikOhNo <80977626+NikOhNo@users.noreply.github.com>
2022-07-09 22:09:23 -0700

    Added Comic and SFX Functionality
    Very cool B]
    -screenshake
    -explosion
    -fading text

---
+++ Anarch-bee/Assets/Scripts/TextFadeOut.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TextFadeOut : MonoBehaviour
+{
+    bool fadedOut = false;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && !fadedOut)
+        {
+            StartCoroutine(FadeTextToZeroAlpha(1f, GetComponent<TMP_Text>()));
+        }
+    }
+    public IEnumerator FadeTextToZeroAlpha(float t, TMP_Text i)
+    {
+        i.color = new Color(i.color.r, i.color.g, i.color.b, 1);
+        while (i.color.a > 0.0f)
+        {
+            i.color = new Color(i.color.r, i.color.g, i.color.b, i.color.a - (Time.deltaTime / t));
+            yield return null;
+        }
+        fadedOut = true;
+    }
+}
